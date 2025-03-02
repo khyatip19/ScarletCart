8e0644b9e487e5e1d0740e277f5b886dc64e8f7c
Sravya <sravya.pogiri@gmail.com>
2025-03-01 20:48:03 -0500

    Initial Commit

---
+++ ScarletCart/ScarletCart/ScarletCartApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScarletCartApp.swift
+//  ScarletCart
+//
+//  Created by Sravya Pogiri on 3/1/25.
+//
+
+import SwiftUI
+
+@main
+struct ScarletCartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ScarletCart/ScarletCartTests/ScarletCartTests.swift
@@ -0,0 +1,16 @@
+//
+//  ScarletCartTests.swift
+//  ScarletCartTests
+//
+//  Created by Sravya Pogiri on 3/1/25.
+//
+
+import Testing
+
+struct ScarletCartTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
