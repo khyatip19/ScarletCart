60bfef8970f230881f76f1b5c309c4ea94946c11
Sravya <sravya.pogiri@gmail.com>
2025-03-01 23:21:41 -0500

    Background - 2

--- ScarletCart/ScarletCart/ContentView.swift
+++ ScarletCart/ScarletCart/ContentView.swift
@@ -9,13 +9,32 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color(red: 235/255, green: 253/255, blue: 242/255)
+                            .edgesIgnoringSafeArea(.all)
+            VStack {
+                Text("Welcome")
+                    .font(Font.custom("Aclonica", size: 52))
+                    .padding(.vertical, 132)
+                NavigationLink(destination: ContentView())
+                {
+                    Text("Log In")
+                        .font(Font.custom("Aclonica", size: 38))
+                        .fontWeight(.bold)
+                        .foregroundColor(.black)
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .cornerRadius(10)
+                }
+                .font(.body)
+                .buttonStyle(.borderedProminent)
+                .tint(Color(UIColor(red: 0.09, green: 0.29, blue: 0.17, alpha: 1)))
+                .padding(.horizontal, 35)
+                .padding(.vertical, 5)
+            }
+            .padding()
         }
-        .padding()
+        
     }
 }
 
